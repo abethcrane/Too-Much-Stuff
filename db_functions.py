910c9a773224d94abfd67a710b8697074bb60864
beth <bethc@cse.unsw.edu.au>
2014-07-09 23:21:31 +1000

    ...tabs

--- db_functions.py
+++ db_functions.py
@@ -31,37 +31,37 @@ def return_items(db, user_id, item_type="Books"):
 def get_user_id(db):
     user_id = None
     app_id = "1492256340991855"
-	app_secret = "9c56bc1468a886c8d7fc986f3c3930e3"
-	
-	# Access token generated from call to below API
-	# https://graph.facebook.com/oauth/access_token?client_id={app-id}&client_secret={app-secret}&grant_type=client_credentials
-	access_token="1492256340991855|W8gMh9CZ6mXCwTUVipyhYopdRVU"
-	
+    app_secret = "9c56bc1468a886c8d7fc986f3c3930e3"
+    
+    # Access token generated from call to below API
+    # https://graph.facebook.com/oauth/access_token?client_id={app-id}&client_secret={app-secret}&grant_type=client_credentials
+    access_token="1492256340991855|W8gMh9CZ6mXCwTUVipyhYopdRVU"
+    
     try:
-    	# Read the auth input token from the fb-set cookie
-		cookie = Cookie.SimpleCookie(os.environ["HTTP_COOKIE"])
-		if "fbsr_1492256340991855" in cookie:
-			auth = cookie["fbsr_1492256340991855"]
-			
-		# Use this to translate the token into (amongst other things) a user id
-		url = 'https://graph.facebook.com/debug_token?input_token={}&access_token={}'.format(auth, access_token)
-		response = urllib2.urlopen(url)
-		data = json.load(response)
-		print data
-	
-		# If we were successful, create a new cookie to store the db user id (not fb), that expires at the same time as the other
-		# Essentially we are caching here for ease
-		if "user_id" in data:
-			user_id = query_db(db, "select User_ID from Users where FB_ID=?", args=(data["user_id"]), one=True)
+        # Read the auth input token from the fb-set cookie
+        cookie = Cookie.SimpleCookie(os.environ["HTTP_COOKIE"])
+        if "fbsr_1492256340991855" in cookie:
+            auth = cookie["fbsr_1492256340991855"]
+            
+        # Use this to translate the token into (amongst other things) a user id
+        url = 'https://graph.facebook.com/debug_token?input_token={}&access_token={}'.format(auth, access_token)
+        response = urllib2.urlopen(url)
+        data = json.load(response)
+        print data
+    
+        # If we were successful, create a new cookie to store the db user id (not fb), that expires at the same time as the other
+        # Essentially we are caching here for ease
+        if "user_id" in data:
+            user_id = query_db(db, "select User_ID from Users where FB_ID=?", args=(data["user_id"]), one=True)
 
-			# This must be a new user, create an entry for them
-			if user_id is None:
-				add_user(db, data["user_id"])
-				user_id = query_db(db, "select User_ID from Users where FB_ID=?", args=(data["user_id"]), one=True)
+            # This must be a new user, create an entry for them
+            if user_id is None:
+                add_user(db, data["user_id"])
+                user_id = query_db(db, "select User_ID from Users where FB_ID=?", args=(data["user_id"]), one=True)
 
-			user_id_cookie = Cookie.SimpleCookie()
-			user_id_cookie["user_ID"] = data["user_id"]
-			user_id_cookie["user_ID"]["expires"] = data["expires_at"]
+            user_id_cookie = Cookie.SimpleCookie()
+            user_id_cookie["user_ID"] = data["user_id"]
+            user_id_cookie["user_ID"]["expires"] = data["expires_at"]
 
     except (Cookie.CookieError, KeyError):
         user_id = -1
@@ -69,4 +69,4 @@ def get_user_id(db):
     return user_id
     
 def add_user(db, fb_id):
-	effect_db(db, "insert into Users(FB_ID) Values(?)", (fb_id))
+    effect_db(db, "insert into Users(FB_ID) Values(?)", (fb_id))
