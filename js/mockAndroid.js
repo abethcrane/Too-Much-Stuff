d4b430eb79b7bf409d4b07c30b49e61a311f3341
beth <bethc@cse.unsw.edu.au>
2014-07-15 14:21:26 +1000

    Update to mockAndroid to only do nothing for scanSomething
    So it plays nicely instead of erroring on web

--- js/mockAndroid.js
+++ js/mockAndroid.js
@@ -3,32 +3,8 @@ Useful for testing in a browser - this removes errors for 'Android not defined' 
 $.Class.extend("MockAndroid", {
 
     init: function () {
-        this.data = '';
     },
 
-    serverSuccess: function (deviceId, name) {
-    },
-
-    serverError: function (deviceId, name) {
-    },
-
-    getServerUrl: function() {
-        return 'localhost';
-    },
-
-    getData: function(roomId, capId) {
-        return this.data;
-    },
-
-    storeData: function(data) {
-        this.data = data;
-    },
-
-    toast: function(data) {
-        alert(data);
-    },
-
-    getToken: function() {
-        return '';
+    scanSomething: function() {
     }
 });
