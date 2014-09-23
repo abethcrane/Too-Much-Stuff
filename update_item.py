09c04c76b31e8845969eb6d030a6c09c1992c156
beth <bethc@cse.unsw.edu.au>
2014-09-23 21:28:26 +1000

    Now let's fix small bugs
    Yay it was file permissions!

--- update_item.py
+++ update_item.py
@@ -10,6 +10,8 @@ from item import *
 
 def update_item(db, item_category, item_unique, column, value, user_id):
 
+    cat_uniques = {"Book": "ISBN", "DVD": "ISBN"}
+
     # Find out item in the database
     item = Item.generic_item(item_unique, item_category, cat_uniques[item_category])
     item_ID = item.get_db_id()
@@ -24,13 +26,13 @@ def main():
    # Get the params
    # Add the user item thing
     print "Content-Type: text/html"
-    print
+
     print "Yay"
     con = sqlite3.connect("test.db")
     with con:
         db = con.cursor()
         user_id = get_user_id(db)
-        print user_id
+
         form = cgi.FieldStorage()
 
         value = col= item_id = None
@@ -40,8 +42,6 @@ def main():
             col = form.getvalue("column")
         if "value" in form:
             value = form.getvalue("value")
-
-        print item_id, col, value
 
         if user_id is not None and item_id is not None and col is not None and value is not None:
             update_item(db, "Book", item_id, col, value, user_id) #Use book as the default item category
