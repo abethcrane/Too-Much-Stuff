7fb80dbb20d20041869a5a77f7e15b2744cfa130
beth <bethc@cse.unsw.edu.au>
2014-09-23 20:43:32 +1000

    Update Item and Add Item to try be better
    Classes and stuff woo

--- add_item.py
+++ add_item.py
@@ -15,100 +15,28 @@ def add_item(db, item_category, item_unique, user_id):
     cat_uniques = {"Book": "ISBN", "DVD": "ISBN"}
 
     # Check if item already exists in items table
-
-    item_exists = query_db(db, "Select * from Items where {0}=?".format(cat_uniques[item_category]), (item_unique,), one=True)
+    item = Item.generic_item(item_unique, item_category, cat_uniques[item_category])
+    item_ID = item.get_db_id()
     does_own = None
 
-    if item_exists is None:
-        #TODO(bethc): Do  a lookup for other relevant fields to insert (e.g. book lookup for title)
+    # If It doesn't exist in the database yet
+    if item_ID is None:
         if item_category == "Book":
-            title, author, image = get_book_data(item_unique)
-            arguments = (item_category, item_unique, title, author, image)
-            effect_db(db, "Insert into Items(Category, ISBN, Title, Author, Image) Values (?, ?, ?, ?, ?)", args=arguments)
+            item = Book(item_unique)
+            item.insert()
         else:
-            effect_db(db, "Insert into Items(Category, {0}) Values (?, ?)".format(cat_uniques[item_category]), args=(item_category, item_unique))
+            item.insert()
+        item_ID = item.get_db_id()
     else:
         # Check the user doesn't already own this, if they do skip adding it
         does_own = query_db(db, "Select * from Owners where User_ID=? and Item_ID=?", (user_id, item_exists["Item_ID"]), one=True)
 
     if does_own is None:
         # Add the entry into users
-        item_id = query_db(db, "Select Item_ID from Items where {0}=?".format(cat_uniques[item_category]), (item_unique,), one=True)
-        if item_id is not None:
-            item_id = item_id["Item_ID"]
+        if item_ID is not None:
             effect_db(db, "Insert into Owners(User_ID, Item_ID) Values (?, ?)", (user_id, item_id))
+        #TODO: else?
 
-
-
-def check_digit(ISBN):
-    total = 0
-    if len(ISBN) is not 9:
-        return ''
-    for i in range (0, 9):
-        total += (10-i) * int(ISBN[i])
-    return str(11 - (total % 11))
-
-def get_ISBN10(ISBN):
-    length = len(ISBN)
-    if length is 9:
-        return ISBN + check_digit(ISBN)
-    elif length is 10:
-        return ISBN
-    elif length is 12 or length is 13:
-        ISBN10 = ISBN[3:12]
-        return ISBN10 + check_digit(ISBN10)
-    else:
-        raise TypeError
-
-def lookup_ISBN(ISBN):
-    try:
-        ISBN10 = get_ISBN10(ISBN)
-        url = "https://www.googleapis.com/books/v1/volumes?q=isbn:{0}".format(ISBN10)
-        data = json.load(urllib2.urlopen(url))
-    except TypeError as e:
-        #print "Error; invalid ISBN"
-        data = None
-    finally:
-        return data
-
-def get_title(data):
-    try:
-        title = data["items"][0]["volumeInfo"]["title"]
-    except Exception as e:
-        title = "Unkown Title"
-    finally:
-        return title
-
-def get_author(data):
-    try:
-        author = data["items"][0]["volumeInfo"]["author"]
-    except Exception as e:
-        try:
-            authors = data["items"][0]["volumeInfo"]["authors"]
-            author = ""
-            for i in authors:
-                author = author + i + ", "
-            author = author[:-2]
-        except Exception as e:
-            author = "Unknown Author"
-    finally:
-        return author
-
-def get_image(data):
-    try:
-        image = data["items"][0]["volumeInfo"]["imageLinks"]["thumbnail"]
-    except Exception as e :
-        image = "http://placehold.it/140x140"
-    finally:
-        return image
-
-def get_book_data(ISBN):
-    data = lookup_ISBN(ISBN)
-    title = get_title(data)
-    author = get_author(data)
-    image = get_image(data)
-
-    return title, author, image
 
 def main():
    # Get the params
--- item.py
+++ item.py
@@ -1,8 +1,17 @@
 class Item(object):
 
-    def __init__(self):
+    def __init__(self, db, identifier=None):
+        self.db = db
+        self.id = identifier
         self.data = {}
         self.table = "Items"
+
+    @classmethod
+    def generic_item(self, identifier, category, id_type):
+        self.__init__(identifier)
+        self.id_type = id_type
+        self.data = {"Category"=category, self.id_type=self.id}
+        #self.table = "{0}s".format(category)
 
     def insert(self):
         question_marks = "?, " * (len(fields)-1) + "?"
@@ -14,10 +23,95 @@ class Item(object):
             fields += (key,)
 
         statement = "Insert into {0}{1} Values ({2})".format(self.table, fields, question_marks)
-        effect_db(db, statement, args=values)
+        effect_db(self.db, statement, args=values)
+
+    def get_db_id(self):
+        id = query_db(self.db, "Select Item_ID from {0} where {1}=?".format(self.table, self.id_type), (self.id,), one=True)
+        if id is None:
+            return id
+        else:
+            return id["Item_ID"]
 
 class Book(Item):
 
-    def __init__(self):
-        self.fields = ("Category", "ISBN", "Title", "Author", "Image")
-        self.table = "Books"
+    def __init__(self, identifier):
+        super(Book, self).__init(identifier)
+        self.id_type="ISBN"
+        self.data["Category"] = "Book"
+        self.data["ISBN"] = self.id
+        self.data["Title"], self.data["Author"], self.data["Image"] = get_book_data(self.id)
+        # TODO: possibly consider self.fields and we can set that to list all the things we want to look up
+        # TODO: For now we don't want to use different tables, because we haven't set that up in the database.
+        #self.table = "Books"
+
+    def exists_in_db(self, identifier, values):
+
+
+    def get_book_data(self, ISBN):
+        data = self.lookup_ISBN(ISBN)
+        title = self. get_title(data)
+        author = self.get_author(data)
+        image = self.get_image(data)
+
+        return title, author, image
+
+    def lookup_ISBN(self, ISBN):
+        try:
+            ISBN10 = self.get_ISBN10(ISBN)
+            url = "https://www.googleapis.com/books/v1/volumes?q=isbn:{0}".format(ISBN10)
+            data = json.load(urllib2.urlopen(url))["items"][0]["volumeInfo"]
+        except TypeError as e:
+            data = None
+        finally:
+            return data
+
+    def check_digit(self, ISBN):
+        total = 0
+        if len(ISBN) is not 9:
+            return ''
+        for i in range (0, 9):
+            total += (10-i) * int(ISBN[i])
+        return str(11 - (total % 11))
+
+    def get_ISBN10(self, ISBN):
+        length = len(ISBN)
+        if length is 9:
+            return ISBN + self.check_digit(ISBN)
+        elif length is 10:
+            return ISBN
+        elif length is 12 or length is 13:
+            ISBN10 = ISBN[3:12]
+            return ISBN10 + self.check_digit(ISBN10)
+        else:
+            raise TypeError
+
+    def get_title(self, data):
+        try:
+            title = data["items"][0]["volumeInfo"]["title"]
+        except Exception as e:
+            title = "Unkown Title"
+        finally:
+            return title
+
+    def get_author(self, data):
+        try:
+            author = data["items"][0]["volumeInfo"]["author"]
+        except Exception as e:
+            try:
+                authors = data["items"][0]["volumeInfo"]["authors"]
+                author = ""
+                for i in authors:
+                    author = author + i + ", "
+                author = author[:-2]
+            except Exception as e:
+                author = "Unknown Author"
+        finally:
+            return author
+
+    def get_image(self, data):
+        try:
+            image = data["items"][0]["volumeInfo"]["imageLinks"]["thumbnail"]
+        except Exception as e :
+            image = "http://placehold.it/140x140"
+        finally:
+            return image
